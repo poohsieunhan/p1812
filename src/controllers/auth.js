fbcaa9ae90e411885ab83aea96c2494ce4717527
itewh <linhdd85@gmail.com>
2023-12-27 15:11:46 +0700

    it_271223

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,21 @@
 import * as services from "../services";
 
-export const regiter =  (req,res)=>{
+export const regiter = async (req,res)=>{
     try {
-        const response = services.register()
+        const {email,password} = req.body;
+        console.log({email,password});
+        if(!email || !password) return res.status(200).json({
+            err: -1,
+            mess: "missing email or pass"
+        })
+
+
+        const response = await services.register(req.body)
+        //console.log(response);
         return res.status(200).json(response)
         
     } catch (error) {
+        console.log(error);
         return res.status(500).json({
             "error" : -1,
             "mess" :'Internal Server Error'
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,13 +1,38 @@
 import db from '../models'
+import bcrypt from 'bcrypt'
 
-export const register = ()=> new Promise((resolve,reject)=>{
+
+
+//const hashPass = password => bcrypt.hashSync(password,bcrypt.genSaltSync(8))
+
+export const register = ({email,password})=> new Promise( async (resolve,reject)=>{
     try {
+        //console.log("2:"+  email,password)
+        const hashPass = await bcrypt.hashSync(password,10)
+        //console.log(hashPass);
+
+        const response = await db.User.findOrCreate({
+            where: {email},
+            defaults:{
+                email,
+                password: hashPass
+            }
+        })
+
+        console.log(response);
+
+        resolve({
+            err: response[1]?0:1,
+            mess: response[1]?'register success':'register fail'
+        })
+
+
         resolve({
             err:0,
             mes:'register service'
         })
 
-        console.log("sfter resolfve");
+        console.log("after resolve");
     } catch (error) {
         reject(error)
     }
